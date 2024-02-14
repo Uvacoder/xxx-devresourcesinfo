d50f117f2f474a372bfa604f6f468907cae16bad
prachi-sahu-codes <prachisahu998@gmail.com>
2024-02-14 12:26:47 +0530

    feat: automatic country and continent update based on filter

--- src/components/modalContainer/pagesDropDown/ConferencesDropDown.jsx
+++ src/components/modalContainer/pagesDropDown/ConferencesDropDown.jsx
@@ -3,6 +3,11 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchConferencesByAllFilter } from "@/redux/features/conference/action";
 import { addQuotesToString } from "@/utils/utils";
+import { getAreaByCity, getAreaByCountry } from "@/services/api/conferenceAPI";
+import {
+  setOtherByCity,
+  setOtherByCountry,
+} from "@/redux/features/conference/conferenceSlice";
 
 const ConferencesDropDown = ({
   obj,
@@ -22,14 +27,21 @@ const ConferencesDropDown = ({
   } = useSelector(({ conferences }) => conferences);
   const dispatch = useDispatch();
 
-  const getData = (dropDownSelected) => {
-    const convertCity = citySelected
-      ? addQuotesToString(citySelected)
-      : undefined;
-    const convertCountry = countrySelected
+  const getData = (dropDownSelected, continent, country) => {
+    const convertCity =
+      continent || country
+        ? undefined
+        : citySelected
+        ? addQuotesToString(citySelected)
+        : undefined;
+    const convertCountry = country
+      ? addQuotesToString(country)
+      : countrySelected
       ? addQuotesToString(countrySelected)
       : undefined;
-    const convertContinent = continentSelected
+    const convertContinent = continent
+      ? addQuotesToString(continent)
+      : continentSelected
       ? addQuotesToString(continentSelected)
       : undefined;
     const convertTech = techSelected
@@ -82,11 +94,42 @@ const ConferencesDropDown = ({
     }
   };
 
+  const setAreaValue = async (id, convertStr) => {
+    const convertId = addQuotesToString(id);
+    if (categorySelected?.name === "City") {
+      const { data } = await getAreaByCity(convertId);
+      dispatch(
+        setOtherByCity({
+          country: data?.country?.name,
+          continent: data?.country?.continent?.name,
+        })
+      );
+      getData(convertStr, data?.country?.continent?.name, data?.country?.name);
+    } else {
+      const { data } = await getAreaByCountry(convertId);
+      dispatch(setOtherByCountry(data?.continent?.name));
+      getData(convertStr, data?.continent?.name);
+    }
+    // getData(convertStr);
+  };
+
   const clickHandler = (e) => {
-    const convertStr = addQuotesToString(e.target.textContent);
+    const valueSelected = e.target.textContent;
+    const convertStr = addQuotesToString(valueSelected);
     handleDropDownSelected(e);
-    getData(convertStr);
-    dispatch(categorySelected.toChangeAtt(e.target.textContent));
+
+    dispatch(
+      categorySelected.toChangeAtt({ value: valueSelected, id: obj?.id })
+    );
+
+    if (
+      categorySelected?.name === "City" ||
+      categorySelected?.name === "Country"
+    ) {
+      setAreaValue(obj?.id, convertStr);
+    } else {
+      getData(convertStr);
+    }
   };
 
   return (
