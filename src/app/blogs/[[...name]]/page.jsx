11e07d61ebb5a0d3a4477536ad860f543153e7db
prachi-sahu-codes <prachisahu998@gmail.com>
2024-02-20 16:58:18 +0530

    feat: local storage removed from blogs page

--- src/app/blogs/[[...name]]/page.jsx
+++ src/app/blogs/[[...name]]/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { DEV_RESOURCES, BLOGS_URL } from "@/utils/constants";
-import { addQuotesToString } from "@/utils/utils";
+import { usePathname } from "next/navigation";
+import Breadcrumb from "@/components/breadcrumb";
 import PageContainer from "@/components/pageContainer";
 import AudienceFilterBar from "@/components/audienceFilterBar";
 import AudienceTable from "@/components/audienceTable";
@@ -11,29 +11,28 @@ import {
   setBlogDataByUrl,
   setBlogStorageData,
 } from "@/redux/features/blog/blogSlice";
+import { addQuotesToString, extractDataFromURL } from "@/utils/utils";
 import { fetchBlogByAllFilter } from "@/redux/features/blog/action";
-import Breadcrumb from "@/components/breadcrumb";
+import { BLOGS_URL } from "@/utils/constants";
+import { fetchFilterFromURL, updateURLAndData } from "@/utils/urlFunc";
 
 const Blogs = ({ name }) => {
   const dispatch = useDispatch();
+  const pathname = usePathname();
+  const dataFromURL = extractDataFromURL(pathname);
   const blogs = useSelector(({ blogs }) => blogs);
   const { allBlogs, status, langSelected, audienceSelected, tagSelected } =
     blogs;
 
-  const fetchData = () => {
-    const localStorageResources =
-      JSON.parse(localStorage.getItem(DEV_RESOURCES)) ?? {};
-
-    dispatch(setBlogStorageData(localStorageResources?.blogs));
-
-    const convertLang = localStorageResources?.blogs?.langSelected
-      ? addQuotesToString(localStorageResources?.blogs?.langSelected)
+  const fetchData = (obj) => {
+    const convertLang = obj?.langSelected
+      ? addQuotesToString(obj?.langSelected)
       : undefined;
-    const convertAudience = localStorageResources?.blogs?.audienceSelected
-      ? addQuotesToString(localStorageResources?.blogs?.audienceSelected)
+    const convertAudience = obj?.audienceSelected
+      ? addQuotesToString(obj?.audienceSelected)
       : undefined;
-    const convertTag = localStorageResources?.blogs?.tagSelected
-      ? addQuotesToString(localStorageResources?.blogs?.tagSelected)
+    const convertTag = obj?.tagSelected
+      ? addQuotesToString(obj?.tagSelected)
       : undefined;
 
     dispatch(
@@ -46,85 +45,29 @@ const Blogs = ({ name }) => {
   };
 
   useEffect(() => {
-    fetchData();
+    const filterFromURL = fetchFilterFromURL(
+      dispatch,
+      setBlogDataByUrl,
+      dataFromURL
+    );
+    fetchData(filterFromURL);
   }, []);
 
   useEffect(() => {
-    if (langSelected && tagSelected && audienceSelected) {
-      window.history.pushState(
-        null,
-        "",
-        `${BLOGS_URL}/${tagSelected}/${audienceSelected}/${langSelected}`
-      );
-      dispatch(
-        setBlogDataByUrl({
-          langSelected,
-          audienceSelected,
-          tagSelected,
-        })
-      );
-    } else if (langSelected && tagSelected) {
-      window.history.pushState(
-        null,
-        "",
-        `${BLOGS_URL}/${tagSelected}/${langSelected}`
-      );
-      dispatch(
-        setBlogDataByUrl({
-          langSelected,
-          tagSelected,
-        })
-      );
-    } else if (tagSelected && audienceSelected) {
-      window.history.pushState(
-        null,
-        "",
-        `${BLOGS_URL}/${tagSelected}/${audienceSelected}`
-      );
-      dispatch(
-        setBlogDataByUrl({
-          audienceSelected,
-          tagSelected,
-        })
-      );
-    } else if (langSelected && audienceSelected) {
-      window.history.pushState(
-        null,
-        "",
-        `${BLOGS_URL}/${audienceSelected}/${langSelected}`
-      );
-      dispatch(
-        setBlogDataByUrl({
-          langSelected,
-          audienceSelected,
-        })
-      );
-    } else if (langSelected) {
-      window.history.pushState(null, "", `${BLOGS_URL}/${langSelected}`);
-      dispatch(
-        setBlogDataByUrl({
-          langSelected,
-          tagSelected,
-        })
-      );
-    } else if (audienceSelected) {
-      window.history.pushState(null, "", `${BLOGS_URL}/${audienceSelected}`);
-      dispatch(
-        setBlogDataByUrl({
-          audienceSelected,
-          tagSelected,
-        })
-      );
-    } else if (tagSelected) {
-      window.history.pushState(null, "", `${BLOGS_URL}/${tagSelected}`);
-      dispatch(
-        setBlogDataByUrl({
-          tagSelected,
-        })
-      );
-    } else {
-      fetchData();
-    }
+    const filterFromURL = fetchFilterFromURL(
+      dispatch,
+      setBlogDataByUrl,
+      dataFromURL
+    );
+    fetchData(filterFromURL);
+  }, [pathname]);
+
+  useEffect(() => {
+    updateURLAndData(BLOGS_URL, fetchData, {
+      langSelected,
+      audienceSelected,
+      tagSelected,
+    });
   }, [langSelected, audienceSelected, tagSelected]);
   return (
     <PageContainer>
