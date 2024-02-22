26a03fbaea65a1d99c1d53b5267cc9dcdaa9de15
prachi-sahu-codes <prachisahu998@gmail.com>
2024-02-22 18:22:22 +0530

    feat: hover styling added

--- src/components/audienceTable/rows/TechnologiesRow.jsx
+++ src/components/audienceTable/rows/TechnologiesRow.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TechnologiesRow = ({ obj, clickHandler, tagSelected }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const darkColor = obj?.darkColor ?? "#B72461";
   const lightColor = obj?.lightColor ?? "#FDE6EB";
 
+  const normalStyle = {
+    backgroundColor: lightColor,
+    color: darkColor,
+  };
+
+  const hoverStyle = {
+    backgroundColor: darkColor,
+    color: "white",
+  };
   return (
     <>
       {tagSelected === obj?.name ? (
@@ -15,16 +25,15 @@ const TechnologiesRow = ({ obj, clickHandler, tagSelected }) => {
           {obj?.name}
         </p>
       ) : (
-        <p
-          className={` text-[11px] xs-450:text-[12px] font-[600] text-center leading-[12px] py-[3px] px-[6px] xs-450:py-[5px] xs-450:px-[8px] rounded-[4px] hover:bg-[${darkColor}] hover:text-white`}
+        <div
+          className={`text-[11px] xs-450:text-[12px] font-[600] text-center leading-[12px] py-[3px] px-[6px] xs-450:py-[5px] xs-450:px-[8px] rounded-[4px] hover:bg-[${darkColor}] hover:text-white`}
           onClick={() => clickHandler(obj?.name, "Tags")}
-          style={{
-            backgroundColor: lightColor,
-            color: darkColor,
-          }}
+          style={isHovered ? { ...normalStyle, ...hoverStyle } : normalStyle}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
         >
           {obj?.name}
-        </p>
+        </div>
       )}
     </>
   );
