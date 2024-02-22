a5b84b57fcc1b20f0c12addc19598105c70469cd
prachi-sahu-codes <prachisahu998@gmail.com>
2024-02-22 17:57:31 +0530

    style: style

--- src/components/audienceTable/rows/TechnologiesRow.jsx
+++ src/components/audienceTable/rows/TechnologiesRow.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 
 const TechnologiesRow = ({ obj, clickHandler, tagSelected }) => {
+  const darkColor = obj?.darkColor ?? "#B72461";
+  const lightColor = obj?.lightColor ?? "#FDE6EB";
+
   return (
     <>
       {tagSelected === obj?.name ? (
         <p
-          className={`text-[11px] xs-450:text-[12px] font-[600] text-center leading-[12px] py-[3px] px-[6px] xs-450:py-[5px] xs-450:px-[8px] rounded-[4px] bg-[${
-            obj?.darkColor ?? "#B72461"
-          }] text-white`}
+          className={`text-[11px] xs-450:text-[12px] font-[600] text-center leading-[12px] py-[3px] px-[6px] xs-450:py-[5px] xs-450:px-[8px] rounded-[4px] text-white`}
           onClick={() => clickHandler(obj?.name, "Tags")}
+          style={{ backgroundColor: darkColor }}
         >
           {obj?.name}
         </p>
       ) : (
         <p
-          className={`text-[11px] xs-450:text-[12px] font-[600] text-center leading-[12px] py-[3px] px-[6px] xs-450:py-[5px] xs-450:px-[8px] rounded-[4px] bg-[${
-            obj?.lightColor ?? "#FDE6EB"
-          }] text-[${obj?.darkColor ?? "#B72461"}] hover:bg-[${
-            obj?.darkColor ?? "#B72461"
-          }] hover:text-white`}
+          className={` text-[11px] xs-450:text-[12px] font-[600] text-center leading-[12px] py-[3px] px-[6px] xs-450:py-[5px] xs-450:px-[8px] rounded-[4px] hover:bg-[${darkColor}] hover:text-white`}
           onClick={() => clickHandler(obj?.name, "Tags")}
+          style={{
+            backgroundColor: lightColor,
+            color: darkColor,
+          }}
         >
           {obj?.name}
         </p>
