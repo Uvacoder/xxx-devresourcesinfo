5a743006bdcc6ed04a95c19470091ad3d2387dc7
prachi-sahu-codes <prachisahu998@gmail.com>
2024-02-22 13:20:57 +0530

    style: footer styling

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="py-[24px] px-[16px]">
-      <p className="text-[14px] text-neutrals-600 text-center">
+      <p className="text-[12px] sm:text-[14px] text-neutrals-600 text-center">
         © 2024 All rights reserved - DevResources from BugFender®
       </p>
     </div>
