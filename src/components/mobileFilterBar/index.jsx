aef03f64a71b6816883fcea35d2d9f2131e3e99a
prachi-sahu-codes <prachisahu998@gmail.com>
2024-02-22 12:31:04 +0530

    style: filter

--- src/components/mobileFilterBar/index.jsx
+++ src/components/mobileFilterBar/index.jsx
@@ -14,12 +14,14 @@ const MobileFilterBar = ({
   const [showFilters, setShowFilters] = useState(false);
 
   return (
-    <div className="block xl:hidden px-5 py-3 text-neutrals-400 border border-indigos-op-100 rounded-[8px] mb-[10px] cursor-pointer">
+    <div className="block xl:hidden px-2 xs-450:px-5 py-2 xs-450:py-3 text-neutrals-400 border border-indigos-op-100 rounded-[8px] mb-[10px] cursor-pointer">
       <div
         className="flex justify-between items-center text-neutrals-500 hover:text-neutrals-700"
         onClick={() => setShowFilters((prev) => !prev)}
       >
-        <span className="font-[500] text-[15px]">Filters</span>
+        <span className="font-[500] text-[13px] xs-450:text-[15px]">
+          Filters
+        </span>
         <IoChevronDownSharp className="p-[1px]" />
       </div>
       {showFilters && (
