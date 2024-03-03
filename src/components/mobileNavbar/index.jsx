57be39b529e5eb17cb1c25166310d1f8e0c8dc22
prachi-sahu-codes <prachisahu998@gmail.com>
2024-03-03 16:37:42 +0530

    style: submit button

--- src/components/mobileNavbar/index.jsx
+++ src/components/mobileNavbar/index.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 import { TbMenu2 } from "react-icons/tb";
+import PrimaryBtn from "../primaryBtn";
 
 const MobileNavbar = ({ menu, pathname }) => {
   const [showMenu, setShowMenu] = useState(false);
@@ -20,24 +21,29 @@ const MobileNavbar = ({ menu, pathname }) => {
       </div>
 
       {showMenu && (
-        <ul className="fixed w-full top-[43px] left-0 flex flex-col items-start gap-[10px] py-4 px-7 sm:p-5 bg-neutral-base text-white">
-          {menu.map((item) => {
-            const isActive =
-              pathname === item.url
-                ? "border-b-4 gradient-border-bottom font-[700]"
-                : "text-neutral-200 font-[400]";
-            return (
-              <li key={item.text}>
-                <a
-                  className={`inline-block text-[18px] no-underline hover:text-white ${isActive}`}
-                  href={item.url}
-                >
-                  {item.text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <div className="fixed w-full top-[43px] left-0 bg-neutral-base text-white">
+          <ul className="flex flex-col items-start gap-[10px] pt-4 px-7 sm:p-5 sm:pb-0">
+            {menu.map((item) => {
+              const isActive =
+                pathname === item.url
+                  ? "border-b-4 gradient-border-bottom font-[700]"
+                  : "text-neutral-200 font-[400]";
+              return (
+                <li key={item.text}>
+                  <a
+                    className={`inline-block text-[18px] no-underline hover:text-white ${isActive}`}
+                    href={item.url}
+                  >
+                    {item.text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="px-5 pt-3 pb-4">
+            <PrimaryBtn text="Submit New" />
+          </div>
+        </div>
       )}
     </div>
   );
