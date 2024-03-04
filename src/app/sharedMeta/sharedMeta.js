30fb75dc7ef246f749ad9b9c4bda2d1ca142c4fb
prachi-sahu-codes <prachisahu998@gmail.com>
2024-03-04 17:56:59 +0530

    feat: robots, twitter added

--- src/app/sharedMeta/sharedMeta.js
+++ src/app/sharedMeta/sharedMeta.js
@@ -4,6 +4,20 @@ export const generateSharedMetaData = (meta) => {
     title: meta.title,
     description: meta.description,
 
+    robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: false,
+        noimageindex: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+
     openGraph: {
       title: meta.title,
       description: meta.description,
@@ -18,6 +32,24 @@ export const generateSharedMetaData = (meta) => {
       ],
       locale: "en_US",
       type: "website",
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: meta.title,
+      description: meta.description,
+      creator: "@bugfenderapp",
+      images: [
+        {
+          url: meta.image ?? "/og-image.png",
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+
+    alternates: {
+      canonical: "https://www.devresources.info/",
     },
 
     icons: [
