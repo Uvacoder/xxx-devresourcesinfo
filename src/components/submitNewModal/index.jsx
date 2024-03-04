d309f455edb0977512fb5cee86126cb64744bbea
prachi-sahu-codes <prachisahu998@gmail.com>
2024-03-04 13:31:03 +0530

    chore: comments removed

--- src/components/submitNewModal/index.jsx
+++ src/components/submitNewModal/index.jsx
@@ -27,7 +27,6 @@ const SubmitNewModal = ({ setShowForm }) => {
         formData?.messageToModerator
       ) {
         setShowForm(false);
-        console.log({ formData });
         handleApiCall(formData);
       }
     } else {
