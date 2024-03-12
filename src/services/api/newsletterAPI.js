401c9e3d58213b86cd80a4be2bf5026d86e70ed1
prachi-sahu-codes <prachisahu998@gmail.com>
2024-03-12 12:53:00 +0530

    feat: newsletters page pagination added

--- src/services/api/newsletterAPI.js
+++ src/services/api/newsletterAPI.js
@@ -1,29 +1,53 @@
 import { getClient } from "../graphQLClient";
-import { allNewsletterFilterQuery } from "../queries/newsletterQueries"; 
+import { allNewsletterFilterQuery } from "../queries/newsletterQueries";
 
 export const getNewsletterByAllFilter = async (
   langSelected,
   audienceSelected,
-  tagSelected
+  tagSelected,
+  endCursor,
+  startCursor,
+  getPage
 ) => {
   const client = getClient(false);
   try {
-    const dataQuery = allNewsletterFilterQuery(
-      langSelected,
-      audienceSelected,
-      tagSelected
-    );
+    let dataQuery;
+
+    if (getPage === "previous") {
+      dataQuery = allNewsletterFilterQuery({
+        langSelected,
+        audienceSelected,
+        tagSelected,
+        startCursor,
+        endCursor: "",
+      });
+    } else {
+      dataQuery = allNewsletterFilterQuery({
+        langSelected,
+        audienceSelected,
+        tagSelected,
+        startCursor: "",
+        endCursor,
+      });
+    }
 
     const gqlResponse = await client.request(dataQuery);
 
     return {
       data: gqlResponse?.allNewsletter?.edges || [],
+      hasEndCursor: gqlResponse?.allNewsletter?.pageInfo?.endCursor,
+      hasStartCursor: gqlResponse?.allNewsletter?.pageInfo?.startCursor,
+      hasNextPage: gqlResponse?.allNewsletter?.pageInfo?.hasNextPage,
+      hasPreviousPage: gqlResponse?.allNewsletter?.pageInfo?.hasPreviousPage,
     };
   } catch (error) {
-    console.error(
-      "Error fetching all newsletter data:",
-      error
-    );
-    return { data: [] };
+    console.error("Error fetching all newsletter data:", error);
+    return {
+      data: [],
+      hasEndCursor: "",
+      hasStartCursor: "",
+      hasNextPage: "",
+      hasPreviousPage: "",
+    };
   }
 };
